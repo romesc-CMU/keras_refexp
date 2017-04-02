69c996e8827636561e2d8e4aadc72f9000a25fb8
ryskina <m.ryskina@gmail.com>
2017-04-02 15:42:56 -0400

    lstm

--- LSTM.py
+++ LSTM.py
@@ -4,59 +4,65 @@ from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.optimizers import RMSprop
-from keras.utils.data_utils import get_file
+from keras.preprocessing import sequence
 from collections import defaultdict
 import numpy as np
-import random
-import sys
-import IPython
+#import random
+#import sys
+#import IPython
 from load_data import load_refexps
 
-train_refexp_list = load_refexps()
-train_refexp_split_list = []
-vocab_dict = {}
+# train_refexp_list = load_refexps()
 train_corpus = []
+sentences = load_refexps()
+sentence_indices = []
+w2i = defaultdict(lambda: len(w2i))
+w2i['<eos>'] = 0
 
-for train_refexp in train_refexp_list:
-    words = train_refexp.split()
-    train_refexp_split_list.append(words)
-    train_corpus += words
+for sentence in sentences:
+    sentence = ['<bos>'] + sentence
+    sentence_indices.append([w2i[word] for word in sentence])
+    #train_corpus += train_refexp
 
-words = sorted(list(set(train_corpus)))
-word_indices = dict((w, i) for i, w in enumerate(words))
-indices_word = dict((i, w) for i, w in enumerate(words))
-IPython.embed()
+#words = sorted(list(set(train_corpus)))
+#words = '<eos>' + '<bos>' + words
+#word_indices = dict((w, i) for i, w in enumerate(words))
+#indices_word = dict((i, w) for i, w in enumerate(words))
+#IPython.embed()
 
-# # cut the text in semi-redundant sequences of maxlen characters
-# maxlen = 40
-# step = 3
-# sentences = []
-# next_chars = []
-# for i in range(0, len(text) - maxlen, step):
-#     sentences.append(text[i: i + maxlen])
-#     next_chars.append(text[i + maxlen])
-# print('nb sequences:', len(sentences))
-#
-# print('Vectorization...')
-# X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-# y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
-# for i, sentence in enumerate(sentences):
-#     for t, char in enumerate(sentence):
-#         X[i, t, char_indices[char]] = 1
-#     y[i, char_indices[next_chars[i]]] = 1
-#
-#
-# # build the model: a single LSTM
-# print('Build model...')
-# model = Sequential()
-# model.add(LSTM(128, input_shape=(maxlen, len(chars))))
-# model.add(Dense(len(chars)))
-# model.add(Activation('softmax'))
-#
-# optimizer = RMSprop(lr=0.01)
-# model.compile(loss='categorical_crossentropy', optimizer=optimizer)
-#
-#
+print("Sentences" + str(len(sentences)))
+
+maxlen = max([len(sentence) for sentence in sentences])
+padded_sentences = sequence.pad_sequence(sentences, maxlen=maxlen, padding='post', dtype='int32', value=w2i['<eos>'])
+
+print('Vectorization...')
+#X = np.zeros((len(sentences), maxlen, len(w2i)), dtype=np.bool)
+#Y = np.zeros((len(sentences), maxlen, len(w2i)), dtype=np.bool)
+X = np.zeros((len(sentences), maxlen))
+Y = np.zeros((len(sentences), maxlen))
+for i, sentence in enumerate(sentences):
+    for t in range(len(sentence)-1):
+#        X[i, t, w2i[sentence[t]]] = 1
+#        Y[i, t, w2i[sentence[t+1]]] = 1
+        X[i, t] = w2i[sentence[t]]
+        Y[i, t] = w2i[sentence[t + 1]]
+    X[i, len(sentence)] = w2i[sentence[-1]]
+    Y[i, len(sentence)] = w2i['<eos>']
+#    X[i, len(sentence), w2i[sentence[-1]]] = 1
+#    Y[i, len(sentence), w2i['<eos>']] = 1
+
+# build the model: a single LSTM
+print('Build model...')
+model = Sequential()
+model.add(Embedding(len(w2i), 1024, input_length=maxlen))
+model.add(LSTM(16, input_shape=(maxlen, 1024)))
+model.add(Dense(len(w2i)))
+model.add(Activation('softmax'))
+
+optimizer = RMSprop(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+
 # def sample(preds, temperature=1.0):
 #     # helper function to sample an index from a probability array
 #     preds = np.asarray(preds).astype('float64')
@@ -65,14 +71,14 @@ IPython.embed()
 #     preds = exp_preds / np.sum(exp_preds)
 #     probas = np.random.multinomial(1, preds, 1)
 #     return np.argmax(probas)
-#
-# # train the model, output generated text after each iteration
-# for iteration in range(1, 60):
-#     print()
-#     print('-' * 50)
-#     print('Iteration', iteration)
-#     loss = model.fit(X, y, batch_size=128, nb_epoch=1)
-#     print(np.exp(2,loss/[len(sent) for sent in sentences]))
+
+# train the model, output generated text after each iteration
+for iteration in range(1, 60):
+    print()
+    print('-' * 50)
+    print('Iteration', iteration)
+    loss = model.fit(X, Y, batch_size=16, nb_epoch=1)
+    print(np.exp(2,loss/[len(sent) for sent in sentences]))
 #     start_index = random.randint(0, len(text) - maxlen - 1)
 #
 #     for diversity in [0.2, 0.5, 1.0, 1.2]:
